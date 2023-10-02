7469cf7542969fd31cbf9d2bcdf4ee5f034878a1
Celina Maria <140039409+Celina-Maria@users.noreply.github.com>
2023-10-02 17:16:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const hourHand = document.querySelector('.hour-hand');
-const minuteHand = document.querySelector('.minute-hand');
-const secondHand = document.querySelector('.second-hand');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
-const getTime = () => {
-    const date = new Date();
-    console.log(date.getHours());
-    console.log(date.getMinutes());
-    console.log(date.getSeconds());
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
 
-    return {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        seconds: date.getSeconds(),
-    }
-}
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
 
-setInterval(() => {
-    const { hours, minutes, seconds } = getTime();
-    
-    secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`;
-    minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`;
-    hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`;
-}, 1000);
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+});
